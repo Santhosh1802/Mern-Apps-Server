86301d5ff8a3d72352a284f35fec7743d3b4bccc
Santhosh1802 <71439086+Santhosh1802@users.noreply.github.com>
2025-06-06 15:17:25 +0530

    error fix

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
+  "https://santhoshkmern.netlify.app/"
 ];
 
 app.use(cors({
